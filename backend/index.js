e5ddcd68a8e8cfd77cdd4ada4bae23bbfa9d0304
Natalievo1 <natalieojeda@icloud.com>
2024-07-11 20:11:46 -0700

    adding default view

--- backend/index.js
+++ backend/index.js
@@ -22,3 +22,13 @@ app.use('/users', require('./controllers/users'))
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
 })
+
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+module.exports.Place = require('./places')
